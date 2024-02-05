66687cff28930c1f89033d75b5525eac76fe7021
Riddhi-chavan <riddhic164@gmail.com>
2024-02-05 15:13:37 +0530

    Changed Resume link from drive

--- src/app/component/Portofolio.js
+++ src/app/component/Portofolio.js
@@ -13,7 +13,7 @@ const Portofolio = () => {
                         </h2>
                         <div className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
                             <span className="m-8 block">
-                                <a href="https://drive.google.com/file/d/1j8-AdDy3EMC66W8FReULSAbVJmAKDIJw/view?usp=sharing" target='_blank' className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 w-72">
+                                <a href="https://drive.google.com/file/d/17QoOqoEncfQZlX77t139PB8L24-T0OOg/view?usp=sharing" target='_blank' className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 w-72">
                                     <span className="absolute left-0 block w-full h-0 transition-all bg-primary opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
                                     <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
