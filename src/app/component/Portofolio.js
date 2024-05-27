b9d7e9aa61d34db0414e7388c3cc1e9257266b25
Riddhi-chavan <riddhic164@gmail.com>
2024-05-27 18:40:16 +0530

    Added digital hippo project

--- src/app/component/Portofolio.js
+++ src/app/component/Portofolio.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Portofolio = () => {
-    return (
-        <div className='bg-black border-b border-white'>
-            <div className="relative z-30" id="portfolio">
-                <div className="container py-16 md:py-20 ">
-                    <div className="mx-auto w-full sm:w-3/4 lg:w-full">
-                        <h2
-                            className="text-center font-header text-4xl font-semibold uppercase text-white sm:text-5xl lg:text-4xl"
-                        >
-                            Check  Resume  for other deatils
-                        </h2>
-                        <div className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
-                            <span className="m-8 block">
-                                <a href="https://drive.google.com/file/d/17QoOqoEncfQZlX77t139PB8L24-T0OOg/view?usp=sharing" target='_blank' className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 w-72">
-                                    <span className="absolute left-0 block w-full h-0 transition-all bg-primary opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-                                    <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-                                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                    </span>
-                                    <span className="relative text-center ml-14">Click here</span>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="bg-black border-b border-white">
+      <div className="relative z-30" id="portfolio">
+        <div className="container py-16 md:py-20 ">
+          <div className="mx-auto w-full sm:w-3/4 lg:w-full">
+            <h2 className="text-center font-header text-4xl font-semibold uppercase text-white sm:text-5xl lg:text-4xl">
+              Check Resume for other deatils
+            </h2>
+            <div className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
+              <span className="m-8 block">
+                <a
+                  href="https://drive.google.com/file/d/1Y5_NqY9BN5HDyesaQeXkxQ1JEoc644DK/view?usp=sharing"
+                  target="_blank"
+                  className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 w-72"
+                >
+                  <span className="absolute left-0 block w-full h-0 transition-all bg-primary opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                  <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+                    <svg
+                      className="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span className="relative text-center ml-14">Click here</span>
+                </a>
+              </span>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Portofolio
+export default Portofolio;
--- src/app/component/Projects.js
+++ src/app/component/Projects.js
@@ -1,15 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const projects = () => {
   return (
-
     <>
-
-      <div className='relative bg-cover bg-center bg-no-repeat  ' style={{ backgroundImage: 'url(img/bg-hero.jpg)' }}>
-        <div className="absolute inset-0 z-20 bg-gradient-to-r from-hero-gradient-from to-hero-gradient-to bg-cover bg-center bg-no-repeat ">
-        </div>
-        <div className="container py-16  text-white relative z-30 " id="projects">
+      <div
+        className="relative bg-cover bg-center bg-no-repeat  "
+        style={{ backgroundImage: "url(img/bg-hero.jpg)" }}
+      >
+        <div className="absolute inset-0 z-20 bg-gradient-to-r from-hero-gradient-from to-hero-gradient-to bg-cover bg-center bg-no-repeat "></div>
+        <div
+          className="container py-16  text-white relative z-30 "
+          id="projects"
+        >
           <h2 className="text-center font-header text-black text-4xl font-semibold uppercase sm:text-5xl lg:text-6xl">
             Check out my Projects
           </h2>
@@ -22,37 +25,66 @@ const projects = () => {
               <div className="text-3xl font-bold mb-2 text-grey-30">
                 Stock Management
               </div>
-              <div className='mb-4 max-w-md'>
-                Efficiently manage your inventory with my stock management website built using Next.js and MongoDB. This user-friendly platform allows you to seamlessly add, edit, and monitor products, quantities, and prices in real-time. The Add to Product component simplifies data entry, and the list view provides quick insights. Experience the power of streamlined inventory management with this Next.js and MongoDB integration.
+              <div className="mb-4 max-w-md">
+                Efficiently manage your inventory with my stock management
+                website built using Next.js and MongoDB. This user-friendly
+                platform allows you to seamlessly add, edit, and monitor
+                products, quantities, and prices in real-time. The Add to
+                Product component simplifies data entry, and the list view
+                provides quick insights. Experience the power of streamlined
+                inventory management with this Next.js and MongoDB integration.
               </div>
-              <a href='https://stock-management-bay.vercel.app/' target='_blank'>
-                <button className='text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95'>
+              <a
+                href="https://stock-management-bay.vercel.app/"
+                target="_blank"
+              >
+                <button className="text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95">
                   View Project
                 </button>
               </a>
             </div>
             <div className="lg:order-2 mt-4 lg:mt-0 transition-transform transform hover:-translate-y-2">
-              <img width={200} height={200} src="/stock.png" alt="Stock Image" className='object-cover w-full h-72 rounded-lg' />
+              <img
+                width={200}
+                height={200}
+                src="/stock.png"
+                alt="Stock Image"
+                className="object-cover w-full h-72 rounded-lg"
+              />
             </div>
           </div>
           <div className="text-white p-4 justify-between flex flex-col lg:flex-row">
             {/* Top: Details */}
             <div className="lg:order-2  order-1 ">
               <div className="text-3xl font-bold mb-2 text-grey-30">
-              Trello-App
+                Trello-App
               </div>
-              <div className='mb-4 max-w-md'>
-
-                Trello-App is your ultimate task management solution, built with Next.js, TypeScript, and Appwrite. Seamlessly organize todos with drag-and-drop columns, utilize powerful search functionality, and leverage ChatGPT for user data summarization. This user-friendly app combines the efficiency of React-Beautiful-DND for smooth task transitions, Tailwind CSS for stunning UIs, and Zustand for lightweight state management. Elevate your productivity with Trello-App modern features, offering an intuitive interface and advanced functionalities for efficient task organization and completion.
+              <div className="mb-4 max-w-md">
+                Trello-App is your ultimate task management solution, built with
+                Next.js, TypeScript, and Appwrite. Seamlessly organize todos
+                with drag-and-drop columns, utilize powerful search
+                functionality, and leverage ChatGPT for user data summarization.
+                This user-friendly app combines the efficiency of
+                React-Beautiful-DND for smooth task transitions, Tailwind CSS
+                for stunning UIs, and Zustand for lightweight state management.
+                Elevate your productivity with Trello-App modern features,
+                offering an intuitive interface and advanced functionalities for
+                efficient task organization and completion.
               </div>
-              <a href='https://trello-app-swart.vercel.app/' target='_blank'>
-                <button className='text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95'>
+              <a href="https://trello-app-swart.vercel.app/" target="_blank">
+                <button className="text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95">
                   View Project
                 </button>
               </a>
             </div>
             <div className="lg:order-1 order-2 mt-6 lg:mt-0 transition-transform transform hover:-translate-y-2 ">
-              <img width={200} height={200} src="/Trello.png" alt="Stock Image" className='object-cover w-full h-72 rounded-lg' />
+              <img
+                width={200}
+                height={200}
+                src="/Trello.png"
+                alt="Stock Image"
+                className="object-cover w-full h-72 rounded-lg"
+              />
             </div>
           </div>
           <div className="text-white p-4 justify-between flex flex-col lg:flex-row">
@@ -61,27 +93,77 @@ const projects = () => {
               <div className="text-3xl font-bold mb-2 text-grey-30">
                 The Live News
               </div>
-              <div className='mb-4 max-w-md'>
-              A dynamic Live News app fueled by the robust Media Stack API. Crafted with Next.js, TypeScript, and featuring dynamic routing, this app offers a seamless news browsing experience. Users can effortlessly explore diverse categories, conduct searches, and switch to a sleek dark mode. The app also enhances readability with a Read More feature that unveils the full article. Engineered for responsiveness and aesthetics, it boasts a blend of technologies, including Tailwind CSS, React-Timago, and HeroIcons. Elevate your news consumption with this immersive and stylish application.
+              <div className="mb-4 max-w-md">
+                A dynamic Live News app fueled by the robust Media Stack API.
+                Crafted with Next.js, TypeScript, and featuring dynamic routing,
+                this app offers a seamless news browsing experience. Users can
+                effortlessly explore diverse categories, conduct searches, and
+                switch to a sleek dark mode. The app also enhances readability
+                with a Read More feature that unveils the full article.
+                Engineered for responsiveness and aesthetics, it boasts a blend
+                of technologies, including Tailwind CSS, React-Timago, and
+                HeroIcons. Elevate your news consumption with this immersive and
+                stylish application.
               </div>
-              <a href='https://the-live-news.vercel.app/' target='_blank'>
-                <button className='text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95'>
+              <a href="https://the-live-news.vercel.app/" target="_blank">
+                <button className="text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95">
                   View Project
                 </button>
               </a>
             </div>
             <div className="lg:order-2 mt-4 lg:mt-0 transition-transform transform hover:-translate-y-2">
-              <img width={200} height={200} src="/TheLiveNews.png" alt="Stock Image" className='object-cover w-full h-72 rounded-lg' />
+              <img
+                width={200}
+                height={200}
+                src="/TheLiveNews.png"
+                alt="Stock Image"
+                className="object-cover w-full h-72 rounded-lg"
+              />
             </div>
           </div>
-
-
-
+          <div className="text-white p-4 justify-between flex flex-col lg:flex-row">
+            {/* Top: Details */}
+            <div className="lg:order-2  order-1 ">
+              <div className="text-3xl font-bold mb-2 text-grey-30">
+                Digital-hippo-marketplace
+              </div>
+              <div className="mb-4 max-w-md">
+                Hippo Marketplace is a sophisticated digital platform where
+                users can purchase UI kits and icon files with real money. The
+                site features a robust seller dashboard and admin panel powered
+                by Payload CMS, alongside secure payment processing via Stripe.
+                Built with Next.js, MongoDB, and Tailwind CSS, the marketplace
+                is fully responsive and efficient. User verification is managed
+                through Nodemailer, ensuring secure transactions. The platform
+                also incorporates Swiper for interactive elements and Zustand
+                for state management, allowing users to effortlessly add,
+                remove, and buy digital assets from their cart. This
+                well-structured site offers a seamless and intuitive experience
+                for both buyers and sellers.
+              </div>
+              <a
+                href="https://digital-hippo-production-526d.up.railway.app/"
+                target="_blank"
+              >
+                <button className="text-white bg-black border-2 border-black w-40 h-14 rounded-full mt-3 hover:bg-primary hover:scale-95">
+                  View Project
+                </button>
+              </a>
+            </div>
+            <div className="lg:order-1 order-2 mt-6 lg:mt-0 transition-transform transform hover:-translate-y-2 ">
+              <img
+                width={200}
+                height={200}
+                src="/Trello.png"
+                alt="Stock Image"
+                className="object-cover w-full h-72 rounded-lg"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default projects
+export default projects;
